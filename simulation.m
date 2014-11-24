c64eb6f7f2bcfb450af69b0c9a2435222a83185d
Yuxin Li <liyx.yuxin@gmail.com>
2014-11-24 15:46:53 -0500

    Fixing code
    To work with a more general price structure

--- simulation.m
+++ simulation.m
@@ -1,9 +1,13 @@
 createHouseholds
 %coefmatrix = [constant coef.numpeople coef.hvalue coef.lawnsize coef.weather coef.income coef.price]
-demandFunc = [-0.157 0.291 0.312 0.0247 0.979 0.15 -0.5];
+demandFunc.hh_coeff = [-0.157 0.291 0.312 0.0247 0.979]'; 
+demandFunc.income = 0.15;
+demandFunc.price = -0.5;
 
-% priceStructure = [upper limits of blocks; price in corresponding block]
-priceStructure = [7500 30000 1000000; 0 4.42 5.25];
+% priceStructure is a column vector who first numBlks elements are the
+% upper limits and whose second numBlks elements are the corresponding
+% prices
+priceStructure = [7500 30000 1000000 0 4.42 5.25]';
 
 
 
